8fa4c2f39f5ae9c341abee5d3d089791e59364e9
james <llljm@163.com>
2021-02-15 22:15:14 +0800

    init gshop

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+      <div id="app">
+        <router-view/>
+        <FooterGuide/>
+
+
+      </div>
+    </div>
+</template>
+
+<script>
+  import FooterGuide from './components/FooterGuide/FooterGuide.vue'
+
+  export default {
+
+    components: {
+      FooterGuide
+    }
+
+  }
+
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  .app
+    width 100%
+    height 100%
+    background #f5f5f5
+
+</style>
